044d942e43ab1ff469ae5c6e515e9197a31236dc
sisy110 <sisy110@gmail.com>
2017-07-29 23:25:29 +0800

    up

--- c/data_type/data_type/main.c
+++ c/data_type/data_type/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+//全局变量
+int global_data; //未初始化的全局变量 bss段  初始值为0
+int global_data2 = 100;//已初始化的全局变量 data段
 int main(void)
 {
 	printf("%d\n", sizeof(int));//4
@@ -18,5 +21,21 @@ int main(void)
 	printf("%hhd\n", ch);//-128 输出8位 1个字节
 	printf("%hd\n", ch);//-128  输出16位 2个字节
 	printf("%hu\n", ch);//65408
+
+	printf("%d\n",sizeof(float));//4
+	printf("%d\n",sizeof(double));//8
+	printf("%d\n",sizeof(long double));//8
+	return 0;
+}
+int main02(void)
+{
+	//变量
+	int n;
+	static int bss_data;//未初始化的静态变量1  bss段
+	static int bss_data2;//未初始化的静态变量2 bss段
+	printf("%p\n",&bss_data);
+	printf("%p\n",&bss_data2);
+	static int data = 100;//已初始化的静态变量 data段
+	printf("hello world\n");
 	return 0;
 }
